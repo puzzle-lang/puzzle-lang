2ca678f04fdbe5a2493b1f21d23929d6f515c11f
李佳伟 <puzzlelang@gmail.com>
2025-12-10 19:53:02 +0800

    feat: gradle 配置优化

--- puzzle-core/build.gradle.kts
+++ puzzle-core/build.gradle.kts
@@ -1,35 +1,30 @@
-import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType
-
 plugins {
-	alias(libs.plugins.kotlin.multiplatform)
-	alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 group = "puzzle.core"
 version = "0.0.0"
 
 kotlin {
-	macosArm64 {
-		binaries {
-			executable {
-				entryPoint = "puzzle.core.main"
-				if (this.buildType == NativeBuildType.RELEASE) {
-					freeCompilerArgs += listOf("-Xallocator=std")
-				}
-			}
-		}
-	}
-	
-	sourceSets {
-		commonMain {
-			dependencies {
-				implementation(libs.bundles.puzzle.core)
-			}
-		}
-		all {
-			languageSettings {
-				enableLanguageFeature("ContextParameters")
-			}
-		}
-	}
+    macosArm64 {
+        binaries {
+            executable {
+                entryPoint = "puzzle.core.main"
+            }
+        }
+    }
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(libs.bundles.puzzle.core)
+            }
+        }
+        all {
+            languageSettings {
+                enableLanguageFeature("ContextParameters")
+            }
+        }
+    }
 }
